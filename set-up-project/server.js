e26e5e40b484a9f72c228e30c1d9abeeb424eeae
JR-PC\meredithjr <meredithpjr@gmail.com>
2021-09-22 20:40:08 -0500

    models

--- set-up-project/server.js
+++ set-up-project/server.js
@@ -1,6 +1,6 @@
 import express from "express";
 import connectDatabase from "./config/db";
-
+import { check, validationResult } from 'express-validation';
 
 // INITIALIZE EXPRESS APPLICATION
 const app = express();
@@ -26,10 +26,24 @@ app.get('/', (req, res) => {
  * @desc Register user 
  */ 
 
-app.post('/api/users', (req, res) => {
-    console.log(req.body);
-    res.send(req.body);
-});
+app.post(
+    '/api/users',
+    [
+        check('name', 'Please enter a valid email').isEmail(),
+        check(
+            'password',
+            'Please enter a password with 6 or more characters'
+        ).isLength({ min: 6})
+    ],
+    (req, res) => {
+        const errors = validationResult(req);
+        if (!error.isEmpty()) {
+            return res.status(422).json({ errors: errors.array() });
+        } else {
+            return res.send(req.body);
+        }
+    }
+);
 
 // CONNECTION LISTENER
 app.listen(3000, () => console.log('Express server running on port 3000'));
